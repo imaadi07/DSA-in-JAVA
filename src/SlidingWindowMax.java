1c650c929b47b352ca9bf3d29428b9794b1d4ba7
Aaditya Bhardwaj <113894836+imaadi07@users.noreply.github.com>
2025-12-25 09:46:49 +0530

    Updated the code of Sliding Window Max

--- src/SlidingWindowMax.java
+++ src/SlidingWindowMax.java
@@ -2,25 +2,32 @@ import java.util.*;
 
 public class SlidingWindowMax {
     public static void main(String[] args) {
-        int[] arr = {2,9,3,8,1,7,12,6,14,4,32,0,7,19,8,12,6};
+        int[] arr = {1,3,-1,-3,5,3,6,7};
         int[] result = maxSlidingWindow(arr, 4);
         System.out.println("Input: "+Arrays.toString(arr));
         System.out.println("Output: "+Arrays.toString(result));
     }
 
     public static int[] maxSlidingWindow(int[] arr, int k) {
+        if(k <= 0 || k > arr.length || arr == null) {
+            return new int[0];
+        }
+
+        if(k == 1) {
+            return arr;
+        }
+
         int[] max = new int[arr.length -k + 1];
-        int i =0, j = k;
+        int i =0, j = k - 1;
 
-
-        while(j < arr.length && i < arr.length - k) {
+        while(j < arr.length) {
             int maxi = Integer.MIN_VALUE;
-            for (int l = i; l < j; l++) {
+            for(int l=i;l<=j; l++) {
                 if(arr[l] > maxi) {
                     maxi = arr[l];
                 }
-                max[i] = maxi;
             }
+            max[i] = maxi;
             i++;
             j++;
         }
